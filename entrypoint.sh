a8e1acf2557c3d897b96fec2c8a9c616116a3ed1
opple6 <132681093+opple6@users.noreply.github.com>
2023-05-23 02:34:31 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'d7b78201-ce99-4a64-9ccd-a0e87af33453'}
+UUID=${UUID:-'12a95e07-38f0-4031-9d6a-ac76eec25e54'}
 
 # 其他Paas保活
-PAAS1_URL= https://test0418.onrender.com
-PAAS2_URL= https://sassy-shard-crocus.glitch.me
+PAAS1_URL= https://test0523.onrender.com
+PAAS2_URL= 
 PAAS3_URL=
 PAAS4_URL=
 PAAS5_URL=
@@ -17,8 +17,8 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH='{"AccountTag":"f69bf3cd59bdc8f8709369e03e7ace8b","TunnelSecret":"UhYRz359CzqG9r7SZVIK17xbjqiJCTmVR7ExRVtcHNM=","TunnelID":"ba6764ca-dc41-4b11-beaa-612a780853b5"}'
-ARGO_DOMAIN=pps.cmkjgfh.cf
+ARGO_AUTH='{"AccountTag":"dfce3efa73422a963969f36f44b9c89b","TunnelSecret":"UfnSg1Ye7ZJoQTZXqWhy0GTeCgh1SSn7uI7WQnveAAI=","TunnelID":"ab7e6d1d-370a-49ad-a2e6-5aa8372a42e4"}'
+ARGO_DOMAIN=vpslog1.bgeuet.pp.ua
 
 generate_config() {
   cat > config.json << EOF
